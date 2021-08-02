25b10e508398b410bea57635e9b971e390b9cc05
Fábio Vieira <43624576+eletricfabio@users.noreply.github.com>
2021-08-02 18:26:22 -0300

    Add files via upload

---
+++ dbo.CategoriaProduto.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CategoriaProduto] (
+    [CategoriaID]        INT           IDENTITY (1, 1) NOT NULL,
+    [CategoriaNome]      VARCHAR (250) NOT NULL,
+    [CategoriaDescricao] VARCHAR (250) NOT NULL,
+    [CategoriaAtivo]     BIT           NULL,
+    PRIMARY KEY CLUSTERED ([CategoriaID] ASC)
+);
+
---
+++ dbo.Produto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Produto] (
+    [ProdutoID]          INT           IDENTITY (1, 1) NOT NULL,
+    [ProdutoNome]        VARCHAR (250) NOT NULL,
+    [ProdutoDescricao]   VARCHAR (250) NOT NULL,
+    [ProdutoAtivo]       BIT           NULL,
+    [ProdutoPerecivel]   BIT           NULL,
+    [ProdutoCategoriaID] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProdutoID] ASC)
+);
